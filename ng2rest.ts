2ddb3601e4500c00ad4afd44869ffbc9c0de3d4a
Levi Fuller <Levi Fuller>
2016-04-25 22:10:06 -0600

    Moved library to a single file

--- ng2rest.ts
+++ ng2rest.ts
@@ -1,2 +1,77 @@
-export {RestService} from './src/rest-api.service';
-export {RestRequest} from './src/rest-request.model'; 
+import { HTTP_PROVIDERS, Http } from 'angular2/http';
+import { Injectable } from 'angular2/core';
+
+export class RestRequest{
+     public parameters: {[name: string]: string} = {};
+   //  public endPoint: string;
+     
+     private _endPoint : string;
+     public get endPoint() : string {
+         return this._endPoint;
+     }
+     public set endPoint(v : string) {
+         if (v.charAt(0) != '/')
+            v = '/' + v;
+         this._endPoint = v;
+     }
+     
+}
+
+@Injectable()
+export class RestService {
+    constructor(private _http: Http) {}
+    // Sent with each request
+    public globalParameters: {[name: string]: string} = {};
+   // public baseUrl: string;
+    
+    private _baseUrl : string;
+    public get baseUrl() : string {
+        return this._baseUrl;
+    }
+    public set baseUrl(v : string) {
+        // remove last '/'
+        if (v.lastIndexOf('/') == v.length - 1)
+            v = v.substr(0, v.length - 1);
+        this._baseUrl = v;
+    }
+    
+    
+    public executeRequest<T>(request: RestRequest) : Promise<T> {
+        var getStatement = this._http.get(this.getFullUrl(request));
+        var promiseToReturn = new Promise<T>(resolve => 
+            getStatement.subscribe((response => {
+                resolve(<T>response.json());
+            })));
+        return promiseToReturn;
+    }
+    
+    getFullUrl(restRequest: RestRequest){
+        var toReturn = this.baseUrl;
+        
+        if (restRequest.endPoint != undefined)
+            toReturn += restRequest.endPoint
+        toReturn += this.getQueryString(restRequest.parameters);
+      
+        return toReturn;
+    }
+    
+    getQueryString(restRequestParameters: {[name: string]: string}): string {
+        var toReturn = '?';
+        var isFirst = true;
+        for (var key in this.globalParameters){
+            if (!isFirst)
+                toReturn += '&';
+            else isFirst = false;
+            toReturn += key + '=' + this.globalParameters[key]
+        }
+        
+        for (var key in restRequestParameters){
+            if (!isFirst)
+                toReturn += '&';
+            else isFirst = false;
+            toReturn += key + '=' + restRequestParameters[key]
+        }
+        
+        return toReturn;
+    }
+}
